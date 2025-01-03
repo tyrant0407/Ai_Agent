4178c30d9f377101d09c97e614a2a0194c20f38f
Aman Kushwaha <amankushwaha1331@gmail.com>
2025-01-04 04:00:22 +0530

    Socketio Autherization Done

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,30 @@ import "dotenv/config";
 import http from "http";
 import app from "./app.js";
 import {Server} from "socket.io";
+import jwt from "jsonwebtoken";
 
 const server = http.createServer(app);
 const io = new Server(server);
 const Port = process.env.PORT || 3000;
+
+io.use((socket,next)=>{
+    try {
+        const token = socket.handshake.auth?.token  || socket.handshake.headers.authorization?.split(" ")[1];
+
+        if(!token){
+            throw new Error("Authentication failed");
+        }
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        if(!decoded){
+            throw new Error("Authentication failed");
+        }
+        socket.user = decoded;
+        next();
+    } catch (error) {
+    next(error);
+    }
+    
+})
 
 
 io.on('connection', socket => {
