e336c1420ecc8ac92a18a57823818d62b4591baa
Aman Kushwaha <amankushwaha1331@gmail.com>
2025-01-14 05:22:23 +0530

    ai adding

--- backend/services/gemini.service.js
+++ backend/services/gemini.service.js
@@ -1,9 +1,10 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI("YOUR_API_KEY");
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_AI_API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-const prompt = "Explain how AI works";
-
-const result = await model.generateContent(prompt);
-console.log(result.response.text());
+export const generateResult = async (prompt) => {   
+    const result = await model.generateContent(prompt);
+    
+    return result.response.text();
+}
